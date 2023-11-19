733b958d2ba7444cdd2e887d0edf6df47dfc592c
Vignesh suresh <133358410+Shazambreath@users.noreply.github.com>
2023-11-19 05:49:35 -0700

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -51,7 +51,7 @@ async def start(client, message):
             InlineKeyboardButton('ℹ️ 𝖧𝖾𝗅𝗉', callback_data='help'),
             InlineKeyboardButton('😊 𝖠𝖻𝗈𝗎𝗍', callback_data='about')
         ]]
-        reply_markup = InlineKeyboardMarkup(buttons)
+          reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
